185d32180347953bbb45f225dce684a75eb2ca3f
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-06 22:45:41 +0800

    added prettier and eslint

--- handler.js
+++ handler.js
@@ -1,17 +1,17 @@
-"use strict";
-const axios = require("axios");
+'use strict';
+const axios = require('axios');
 
 module.exports.remind = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event
       },
       null,
       2
-    ),
+    )
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
